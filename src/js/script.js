f0e7f1154e311db1442d6cae492606e75197a151
Pedro <pedro.manoel201599@gmail.com>
2026-09-16 19:02:28 -0300

    chore: adicionando as imagens dos produtos

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+let imagens=[
+    "/src/assets/flor.png",
+    "/src/assets/hilda.png",
+    "/src/assets/palhaço.png",
+    "/src/assets/rato.png",
+    "/src/assets/robo.png"
+];
+
+let index=0;
+
+let tempo = 3000; 
+
+
+function SlideShow(){
+
+    document.getElementById("imgBanner").src=imagens[index];
+
+    index++;
+
+    if(index == imagens.length){
+        index=0;
+    }
+    setTimeout("SlideShow()",tempo)
+}
+//EXECUTAR A FUNÇÃO
+SlideShow();
+const menuIcone = document.getElementById("menu-icone");
+const navMenu = document.getElementById("nav-menu");
+
+menuIcone.addEventListener("click",()=>{
+    navMenu.classList.toggle("active");
+    menuIcone.classList.toggle("open")
+})
